164b06c1e6e6d4ad4731f9cd214d5af89a232d1c
gabster-tech <84643056+gabster-tech@users.noreply.github.com>
2022-11-13 20:13:34 +0000

    Update world.php

--- world.php
+++ world.php
@@ -16,3 +16,21 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php endforeach; ?>
 </ul>
 
+?>
+<table>
+  <thead>
+    <th>Country Name </th>
+    <th>Continent </th>
+    <th>Independence </th>
+    <th>Head of State </th>
+  </thead>
+<?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name']; ?> </td>
+    <td><?= $row['continent']; ?> </td>
+    <td><?= $row['independence_year']; ?> </td>
+    <td><?= $row['head_of_state']; ?> </td>
+  </tr>
+<?php endforeach; ?>
+  </thead>
+</table>
